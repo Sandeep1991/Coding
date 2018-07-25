1432ccb3638f9e42e1ee4f5673b82f0cf4d3c9f8
Sandeep1991 <sandeepvadiraj@yahoo.co.in>
2018-07-24 21:33:28 -0700

    Create Dijkstra.cpp

---
+++ Dijkstra.cpp
@@ -0,0 +1,59 @@
+#ifdef MAC
+#include <stdc++.h>
+#else
+#include <bits/stdc++.h>
+#endif
+
+#include <unordered_map>
+
+using namespace std;
+//https://www.quora.com/What-is-the-most-simple-efficient-C++-code-for-Dijkstras-shortest-path-algorithm
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector< pii > vii;
+#define INF 0x3f3f3f3f
+ 
+vii *G;   // Graph
+vi Dist;  // for storing the distance of every other node from source.
+/*==========================================*/
+void Dijkstra(int source, int N) {
+    priority_queue<pii, vector<pii>, greater<pii> > Q;   // min heap
+    Dist.assign(N,INF);
+    Dist[source] = 0;
+    Q.push({0,source});
+    while(!Q.empty()){
+        int u = Q.top().second;
+        Q.pop();
+        for(auto &c : G[u]){
+            int v = c.first;
+            int w = c.second;
+            if(Dist[v] > Dist[u]+w){
+                Dist[v] = Dist[u]+w;
+                Q.push({Dist[v],v});
+            }
+        }
+    }
+}
+
+int main()
+{
+	int n,m;
+	cin>>n>>m;
+	G = new vii[n+1];
+	
+	int u,v,w,s;
+	for (int i=0;i<m;i++)
+	{
+		cin>>u>>v>>w;
+		G[u].push_back({v,w});
+		//G[v].push_back({u,w});
+	}
+	s = 1;
+	Dijkstra(s,n+1);
+	for (int i=2;i<=n;i++)
+	{
+		cout<<Dist[i]<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
